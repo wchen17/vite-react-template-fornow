d002db88c9ea9cba0ac2b9fc0b2712ed36f27e4f
wchen17 <142947238+wchen17@users.noreply.github.com>
2025-07-12 11:47:37 -0500

    Update App.tsx
    emptied

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,1 +1,20 @@
 
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
+import honoLogo from "./assets/hono.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("unknown");
+
+  return (
+    <div>
+      <h1>My Awesome Website</h1>
+    </div>
+  )
+}
+
+export default App
